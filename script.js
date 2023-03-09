0aea47a56abf24ff4666034f5af5cebf7575a2aa
rinjhasijapati <aastha_sijapati@yahoo.com>
2023-03-09 19:07:49 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function bulbOn(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "image/bulb-on.png");
+}
+
+function bulbOff(){
+    let bulbobj1 = document.querySelector(".bulboff");
+    bulbobj1.setAttribute("src", "image/bulb-off.png");
+}
